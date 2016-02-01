8af822ef694fd8929e93994856e3c3f78978f537
Kenny Tran <kenny8tran@gmail.com>
2016-01-31 22:30:00 -0800

    Restructure tests into describe blocks

--- test/purify_test.js
+++ test/purify_test.js
@@ -8,127 +8,244 @@ var read = function (path) {
 }
 
 describe('purify', function(){
-  it('can find classes intact', function(){
-    var content = read('simple/simple.js');
-    var css = read('simple/simple.css');
-    var result = purify(content, css);
+  describe('find intact classes', function () {
+    beforeEach(function () {
+      var content = read('simple/simple.js');
+      var css = read('simple/simple.css');
+      this.result = purify(content, css);
+    });
 
-    expect(result.indexOf('.single') > -1).to.equal(true);
-    expect(result.indexOf('.double-class') > -1).to.equal(true);
-    expect(result.indexOf('.triple-simple-class') > -1).to.equal(true);
-  });
+    it('finds .single', function () {
+      expect(this.result.indexOf('.single') > -1).to.equal(true);
+    });
 
-  it('can use a callback', function(){
-    var content = read('simple/simple.js');
-    var css = read('simple/simple.css');
+    it('finds .double-class', function () {
+      expect(this.result.indexOf('.double-class') > -1).to.equal(true);
+    });
 
-    purify(content, css, function(result){
-      expect(result.indexOf('.single') > -1).to.equal(true);
-      expect(result.indexOf('.double-class') > -1).to.equal(true);
-      expect(result.indexOf('.triple-simple-class') > -1).to.equal(true);
+    it('can find .triple-simple-class', function(){
+      expect(this.result.indexOf('.triple-simple-class') > -1).to.equal(true);
     });
   });
 
-  it('can find classes that are added together', function(){
-    var content = read('combined/combined.js');
-    var css = read('combined/combined.css');
-    var result = purify(content, css);
+  describe('callback', function () {
+    beforeEach(function () {
+      this.content = read('simple/simple.js');
+      this.css = read('simple/simple.css');
+    });
 
-    expect(result.indexOf('.added-together') > -1).to.equal(true);
-    expect(result.indexOf('.array-joined') > -1).to.equal(true);
+    it('can use a callback', function(){
+      purify(this.content, this.css, function(result){
+        expect(result.indexOf('.triple-simple-class') > -1).to.equal(true);
+      });
+    });
   });
 
-  it('filters out classes not used', function(){
-    var content = read('remove_unused/remove_unused.js');
-    var css = read('remove_unused/remove_unused.css');
-    var result = purify(content, css);
+  describe('classes that are added together', function () {
+    beforeEach(function () {
+      var content = read('combined/combined.js');
+      var css = read('combined/combined.css');
+      this.result = purify(content, css);
+    });
 
-    expect(result.indexOf('.used-class') > -1).to.equal(true);
-    expect(result.indexOf('.unused-class') === -1).to.equal(true);
-    expect(result.indexOf('.another-one-not-found') === -1).to.equal(true);
+    it('can find .added-together', function () {
+      expect(this.result.indexOf('.added-together') > -1).to.equal(true);
+    });
+
+    it('can find .array-joined', function(){
+      expect(this.result.indexOf('.array-joined') > -1).to.equal(true);
+    });
   });
 
-  it('works with multiple files', function(){
-    var content = [absPath + 'multiple_files/multiple_files.js', absPath + 'multiple_files/multiple_files.html'];
-    var css = [absPath + 'multiple_files/multiple_files.css', absPath + 'multiple_files/multiple_files2.css'];
-    var result = purify(content, css);
+  describe('filters out unused selectors', function () {
+    beforeEach(function () {
+      var content = read('remove_unused/remove_unused.js');
+      var css = read('remove_unused/remove_unused.css');
+      this.result = purify(content, css);
+    });
 
-    expect(result.indexOf('.taylor-swift') > -1).to.equal(true);
-    expect(result.indexOf('.blank-space') > -1).to.equal(true);
-    expect(result.indexOf('.shake-it-off') === -1).to.equal(true);
+    it('contains .used-class', function () {
+      expect(this.result.indexOf('.used-class') > -1).to.equal(true);
+    });
+
+    it('removes .unused-class', function () {
+      expect(this.result.indexOf('.unused-class') === -1).to.equal(true);
+    });
+
+    it('removes .another-one-not-found', function () {
+      expect(this.result.indexOf('.another-one-not-found') === -1).to.equal(true);
+    });
   });
 
-  it('works with camelCase', function(){
-    var content = read('camel_case/camel_case.js');
-    var css = read('camel_case/camel_case.css');
-    var result = purify(content, css);
+  describe('works with multiple files', function () {
+    beforeEach(function () {
+      var content = [absPath + 'multiple_files/multiple_files.js', absPath + 'multiple_files/multiple_files.html'];
+      var css = [absPath + 'multiple_files/multiple_files.css', absPath + 'multiple_files/multiple_files2.css'];
+      this.result = purify(content, css);
+    });
 
-    expect(result.indexOf('testFoo') > -1).to.equal(true);
-    expect(result.indexOf('camelCase') > -1).to.equal(true);
+    it('finds .taylor-swift', function () {
+      expect(this.result.indexOf('.taylor-swift') > -1).to.equal(true);
+    });
+
+    it('finds .blank-space', function () {
+      expect(this.result.indexOf('.blank-space') > -1).to.equal(true);
+    });
+
+    it('removes .shake-it-off', function () {
+      expect(this.result.indexOf('.shake-it-off') === -1).to.equal(true);
+    });
   });
 
-  it('works with wildcard, pseudo-elements', function(){
-    var content = read('wildcard/wildcard.html');
-    var css = read('wildcard/wildcard.css');
-    var result = purify(content, css);
+  describe('camelCase', function () {
+    beforeEach(function () {
+      var content = read('camel_case/camel_case.js');
+      var css = read('camel_case/camel_case.css');
+      this.result = purify(content, css);
+    });
 
-    expect(result.indexOf('*') > -1).to.equal(true);
-    expect(result.indexOf('before') > -1).to.equal(true);
-    expect(result.indexOf('scrollbar') > -1).to.equal(true);
-    expect(result.indexOf('selection') > -1).to.equal(true);
-    expect(result.indexOf('vertical') > -1).to.equal(true);
-    expect(result.indexOf(':root') > -1).to.equal(true);
+    it('finds testFoo', function () {
+      expect(this.result.indexOf('testFoo') > -1).to.equal(true);
+    });
+
+    it('finds camelCase', function () {
+      expect(this.result.indexOf('camelCase') > -1).to.equal(true);
+    });
   });
 
-  it('works with media queries', function(){
-    var content = read('media_queries/media_queries.html');
-    var css = read('media_queries/media_queries.css');
-    var result = purify(content, css);
+  describe('wildcard', function () {
+    beforeEach(function () {
+      var content = read('wildcard/wildcard.html');
+      var css = read('wildcard/wildcard.css');
+      this.result = purify(content, css);
+    });
 
-    expect(result.indexOf('.media-class') > -1).to.equal(true);
-    expect(result.indexOf('.alone') > -1).to.equal(true);
-    expect(result.indexOf('#id-in-media') > -1).to.equal(true);
-    expect(result.indexOf('body') > -1).to.equal(true);
-    expect(result.indexOf('.unused-class') > -1).to.equal(false);
-    expect(result.indexOf('66666px') > -1).to.equal(false);
+    it('finds universal selector', function () {
+      expect(this.result.indexOf('*') > -1).to.equal(true);
+    });
+
+    it('finds :before', function () {
+      expect(this.result.indexOf('before') > -1).to.equal(true);
+    });
+
+    it('finds scrollbar', function () {
+      expect(this.result.indexOf('scrollbar') > -1).to.equal(true);
+    });
+
+    it('finds selection', function () {
+      expect(this.result.indexOf('selection') > -1).to.equal(true);
+    });
+
+    it('finds vertical', function () {
+      expect(this.result.indexOf('vertical') > -1).to.equal(true);
+    });
+
+    it('finds :root', function () {
+      expect(this.result.indexOf(':root') > -1).to.equal(true);
+    });
   });
 
-  it('works with attribute selectors', function(){
-    var content = read('attribute_selector/attribute_selector.js');
-    var css = read('attribute_selector/attribute_selector.css');
-    var result = purify(content, css);
+  describe('media queries', function () {
+    beforeEach(function () {
+      var content = read('media_queries/media_queries.html');
+      var css = read('media_queries/media_queries.css');
+      this.result = purify(content, css);
+    });
 
-    expect(result.indexOf('font-icon-') > -1).to.equal(true);
-    expect(result.indexOf('center aligned') > -1).to.equal(true);
-    expect(result.indexOf('github') > -1).to.equal(false);
+    it('finds .media-class', function () {
+      expect(this.result.indexOf('.media-class') > -1).to.equal(true);
+    });
+
+    it('finds .alone', function () {
+      expect(this.result.indexOf('.alone') > -1).to.equal(true);
+    });
+
+    it('finds #id-in-media', function () {
+      expect(this.result.indexOf('#id-in-media') > -1).to.equal(true);
+    });
+
+    it('finds body', function () {
+      expect(this.result.indexOf('body') > -1).to.equal(true);
+    });
+
+    it('removes .unused-class', function () {
+      expect(this.result.indexOf('.unused-class') > -1).to.equal(false);
+    });
+
+    it('removes the empty media query', function () {
+      expect(this.result.indexOf('66666px') > -1).to.equal(false);
+    });
   });
 
-  it('works with special characters', function(){
-    var content = read('special_characters/special_characters.js');
-    var css = read('special_characters/special_characters.css');
-    var result = purify(content, css);
+  describe('attribute selectors', function () {
+    beforeEach(function () {
+      var content = read('attribute_selector/attribute_selector.js');
+      var css = read('attribute_selector/attribute_selector.css');
+      this.result = purify(content, css);
+    });
 
-    expect(result.indexOf('@home') > -1).to.equal(true);
-    expect(result.indexOf('+rounded') > -1).to.equal(true);
-    expect(result.indexOf('button') > -1).to.equal(true);
+    it('finds font-icon-', function () {
+      expect(this.result.indexOf('font-icon-') > -1).to.equal(true);
+    });
+
+    it('finds center aligned', function () {
+      expect(this.result.indexOf('center aligned') > -1).to.equal(true);
+    });
+
+    it('does not find github', function () {
+      expect(this.result.indexOf('github') > -1).to.equal(false);
+    });
   });
 
-  it('works with delimited selectors', function (){
-    var content = read('delimited/delimited.html');
-    var css = read('delimited/delimited.css');
-    var result = purify(content, css);
+  describe('special characters', function () {
+    beforeEach(function () {
+      var content = read('special_characters/special_characters.js');
+      var css = read('special_characters/special_characters.css');
+      this.result = purify(content, css);
+    });
 
-    var commaCount = result.split('').reduce(function (total, chr){
-      if (chr === ','){
-        return total + 1;
-      }
+    it('finds @home', function () {
+      expect(this.result.indexOf('@home') > -1).to.equal(true);
+    });
 
-      return total;
-    }, 0);
+    it('finds +rounded', function () {
+      expect(this.result.indexOf('+rounded') > -1).to.equal(true);
+    });
 
-    expect(commaCount).to.equal(1);
-    expect(result.indexOf('h1') > -1).to.equal(true);
-    expect(result.indexOf('p') > -1).to.equal(true);
-    expect(result.indexOf('.unused-class-name') === -1).to.equal(true);
+    it('finds button', function () {
+      expect(this.result.indexOf('button') > -1).to.equal(true);
+    });
+  });
+
+  describe('delimited', function () {
+    beforeEach(function () {
+      var content = read('delimited/delimited.html');
+      var css = read('delimited/delimited.css');
+      this.result = purify(content, css);
+    });
+
+    it('removes the extra comma', function () {
+      var commaCount = this.result.split('').reduce(function (total, chr){
+        if (chr === ','){
+          return total + 1;
+        }
+
+        return total;
+      }, 0);
+
+      expect(commaCount).to.equal(1);
+    });
+
+    it('finds h1', function () {
+      expect(this.result.indexOf('h1') > -1).to.equal(true);
+    });
+
+    it('finds p', function () {
+      expect(this.result.indexOf('p') > -1).to.equal(true);
+    });
+
+    it('removes .unused-class-name', function () {
+      expect(this.result.indexOf('.unused-class-name') === -1).to.equal(true);
+    });
   });
 });
